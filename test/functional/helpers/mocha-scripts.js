443f58ea672cc53429b95feb34d8c9b41fa9da02
dan@saucelabs.com <dan@saucelabs.com>
2017-09-07 13:17:12 -0700

    Refactor to use appium test support 0.5.0

--- test/functional/helpers/mocha-scripts.js
+++ test/functional/helpers/mocha-scripts.js
@@ -8,20 +8,20 @@ import wd from 'wd';
 import { startServer, DEFAULT_PORT } from '../../..';
 import logger from '../../../lib/logger';
 
-(async function () {
-  if (process.env.TESTOBJECT_E2E_TESTS) {
-    logger.debug('Running tests on TestObject');
+if (process.env.TESTOBJECT_E2E_TESTS) {
+  logger.debug('Running tests on TestObject');
 
-    let wdObject;
-    before(async function () {
-      const branch = process.env.TRAVIS_BRANCH || 'master';
-      wdObject = await enableTestObject(wd, 'appium-uiautomator2-driver', `git@github.com:appium/appium-uiautomator2-driver.git#${branch}`);
-    });
-    after(async function () {
-      await disableTestObject(wdObject);
-    });
+  let wdObject;
+  before(async function () {
+    const branch = process.env.TRAVIS_BRANCH || 'master';
+    wdObject = await enableTestObject(wd, 'appium-uiautomator2-driver', `git@github.com:appium/appium-uiautomator2-driver.git#${branch}`);
+  });
+  after(async function () {
+    await disableTestObject(wdObject);
+  });
 
-  } else {
+} else {
+  before(async function () {
     await startServer(DEFAULT_PORT, 'localhost');
-  }
-})();
+  });
+}
