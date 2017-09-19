e90154f41ab76d43e1b29a1fb018b463a33207db
dan@saucelabs.com <dan@saucelabs.com>
2017-09-19 11:38:26 -0700

    Squash: Using https instead of git

--- test/functional/helpers/mocha-scripts.js
+++ test/functional/helpers/mocha-scripts.js
@@ -17,7 +17,7 @@ if (process.env.TESTOBJECT_E2E_TESTS) {
     if (!branch) {
       throw new Error(`A commit must be provided in $COMMIT_HASH`);
     }
-    wdObject = await enableTestObject(wd, 'appium-uiautomator2-driver', `git@github.com:appium/appium-uiautomator2-driver.git#${branch}`);
+    wdObject = await enableTestObject(wd, 'appium-uiautomator2-driver', `https://github.com/appium/appium-uiautomator2-driver.git`);
   });
   after(async function () {
     await disableTestObject(wdObject);
