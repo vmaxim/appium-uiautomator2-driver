a7e7485f3fca2d3ad6359a34ef9599fbac16f883
vmaxim <vmaximwork@gmail.com>
2017-10-31 14:54:35 +0300

    Add picker tests

--- test/functional/commands/element-e2e-specs.js
+++ test/functional/commands/element-e2e-specs.js
@@ -8,13 +8,15 @@ import { initDriver } from '../helpers/session';
 chai.should();
 chai.use(chaiAsPromised);
 
+const PACKAGE = APIDEMOS_CAPS.appPackage;
+const TEXT_FIELDS_ACTIVITY = '.view.TextFields';
+const DATE_WIDGETS_ACTIVITY = '.view.DateWidgets1';
+const CUSTOM_PICKER_ACTIVITY ='.view.CustomPicker1';
+
 describe('element', function () {
   let driver;
-  let el;
   before(async () => {
-    driver = await initDriver(Object.assign({}, APIDEMOS_CAPS, {appActivity: '.view.TextFields'}));
-
-    el = _.last(await driver.elementsByClassName('android.widget.EditText'));
+    driver = await initDriver(Object.assign({}, APIDEMOS_CAPS));
   });
   after(async () => {
     await driver.quit();
@@ -22,8 +24,44 @@ describe('element', function () {
 
   describe('setValue', () => {
     it('should set the text on the element', async () => {
+      await driver.startActivity({appPackage: PACKAGE, appActivity: TEXT_FIELDS_ACTIVITY});
+      let el = _.last(await driver.elementsByClassName('android.widget.EditText'));
       await el.sendKeys('original value');
       await el.text().should.eventually.equal('original value');
+    });
+    it('should be able to set text in picker', async () => {
+      await driver.startActivity({appPackage: PACKAGE, appActivity: DATE_WIDGETS_ACTIVITY});
+      await (await driver.elementById('io.appium.android.apis:id/pickTimeSpinner')).click();
+
+      let hours = await driver.waitForElementByXPath('//android.widget.NumberPicker[1]//android.widget.EditText');
+      await hours.sendKeys('9');
+      // Force get focus event
+      await hours.click();
+
+      let minutes = await driver.elementByXPath('//android.widget.NumberPicker[2]//android.widget.EditText');
+      await minutes.sendKeys('59');
+      // Force get focus event
+      await minutes.click();
+
+      let pm = await driver.elementByXPath('//*[@text="PM"]');
+      await pm.click();
+
+      let okButton = await driver.elementByXPath('//*[@text="OK" or @text="Ok"]');
+      await okButton.click();
+
+      let date = await driver.waitForElementById('io.appium.android.apis:id/dateDisplay');
+      await date.text().should.eventually.contains('21:59');
+    });
+    it('should be able to set text in picker with custom displayed values', async () => {
+      await driver.startActivity({appPackage: PACKAGE, appActivity: CUSTOM_PICKER_ACTIVITY});
+      let picker = await driver.elementByXPath('//android.widget.NumberPicker[1]//android.widget.EditText');
+
+      await picker.sendKeys('kupima');
+      // Force get focus event
+      await picker.click();
+
+      let textView = await driver.elementById('io.appium.android.apis:id/textView1');
+      await textView.text().should.eventually.contains('kupima');
     });
   });
 });
